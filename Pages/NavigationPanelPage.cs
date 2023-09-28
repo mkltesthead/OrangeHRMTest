417bd18fe3b8566b4d102027b4c6b2e15321f299
Michael Larsen <mlarsen.c@xpanxion.com>
2023-09-27 22:23:54 -0700

    Filled out the Navigation Panel and Navigation Tests

--- Pages/NavigationPanelPage.cs
+++ Pages/NavigationPanelPage.cs
@@ -9,12 +9,6 @@
         public const string AdminSelector = "text=Admin";
         public const string AdminHeaderSelector = "h6.oxd-text:has-text('Admin')";
 
-        public const string DirectorySelector = "text=Directory";
-        public const string DirectoryHeaderSelector = "h6.oxd-text:has-text('Directory')";
-
-        public const string DashboardSelector = "text=Dashboard";
-        public const string DashboardHeaderSelector = "h6.oxd-text:has-text('Dashboard')";
-
         public const string PIMSelector = "text=PIM";
         public const string PIMHeaderSelector = "h6.oxd-text:has-text('PIM')";
 
@@ -23,6 +17,30 @@
 
         public const string TimeSelector = "text=Time";
         public const string TimeHeaderSelector = "h6.oxd-text:has-text('Time')";
+
+        public const string RecruitmentSelector = "text=Recruitment";
+        public const string RecruitmentHeaderSelector = "h6.oxd-text:has-text('Recruitment')";
+
+        public const string MyInfoSelector = "text=My Info";
+        public const string MyInfoHeaderSelector = "h6.oxd-text:has-text('PIM')";
+
+        public const string PerformanceSelector = "text=Performance";
+        public const string PerformanceHeaderSelector = "h6.oxd-text:has-text('Performance')";
+
+        public const string DashboardSelector = "text=Dashboard";
+        public const string DashboardHeaderSelector = "h6.oxd-text:has-text('Dashboard')";
+
+        public const string DirectorySelector = "text=Directory";
+        public const string DirectoryHeaderSelector = "h6.oxd-text:has-text('Directory')";
+
+        public const string MaintenanceSelector = "text=Maintenance";
+        public const string MaintenanceHeaderSelector = "h6.oxd-text:has-text('Administrator Access')";
+
+        public const string ClaimSelector = "text=Claim";
+        public const string ClaimHeaderSelector = "h6.oxd-text:has-text('Claim')";
+
+        public const string BuzzSelector = "text=Buzz";
+        public const string BuzzHeaderSelector = "h6.oxd-text:has-text('Buzz')";
 
         public NavigationPanelPage(IPage page)
         {
@@ -38,27 +56,6 @@
         {
             return await _page.IsVisibleAsync(AdminHeaderSelector);
         }
-
-        // Directory Element Methods
-        public async Task GoToDirectoryPageAsync()
-        {
-            await _page.ClickAsync(DirectorySelector);
-        }
-        public async Task<bool> IsDirectoryHeaderVisibleAsync()
-        {
-            return await _page.IsVisibleAsync(DirectoryHeaderSelector);
-        }
-
-        // Dashboard Element Methods
-        public async Task GoToDashboardPageAsync()
-        {
-            await _page.ClickAsync(DashboardSelector);
-        }
-        public async Task<bool> IsDashboardHeaderVisibleAsync()
-        {
-            return await _page.IsVisibleAsync(DashboardHeaderSelector);
-        }
-
         // PIM Element Methods
         public async Task GoToPIMPageAsync()
         {
@@ -78,7 +75,6 @@
         {
             return await _page.IsVisibleAsync(LeaveHeaderSelector);
         }
-        // ... more methods based on further interactions or validations.
 
         // Time Element Methods
         public async Task GoToTimePageAsync()
@@ -88,6 +84,86 @@
         public async Task<bool> IsTimeHeaderVisibleAsync()
         {
             return await _page.IsVisibleAsync(TimeHeaderSelector);
+        }
+
+        // Recruitment Element Methods
+        public async Task GoToRecruitmentPageAsync()
+        {
+            await _page.ClickAsync(RecruitmentSelector);
+        }
+        public async Task<bool> IsRecruitmentHeaderVisibleAsync()
+        {
+            return await _page.IsVisibleAsync(RecruitmentHeaderSelector);
+        }
+
+        // My Info Element Methods
+        public async Task GoToMyInfoPageAsync()
+        {
+            await _page.ClickAsync(MyInfoSelector);
+        }
+        public async Task<bool> IsMyInfoHeaderVisibleAsync()
+        {
+            return await _page.IsVisibleAsync(MyInfoHeaderSelector);
+        }
+
+        // My Info Element Methods
+        public async Task GoToPerformancePageAsync()
+        {
+            await _page.ClickAsync(PerformanceSelector);
+        }
+        public async Task<bool> IsPerformanceHeaderVisibleAsync()
+        {
+            return await _page.IsVisibleAsync(PerformanceHeaderSelector);
+        }
+
+        // Dashboard Element Methods
+        public async Task GoToDashboardPageAsync()
+        {
+            await _page.ClickAsync(DashboardSelector);
+        }
+        public async Task<bool> IsDashboardHeaderVisibleAsync()
+        {
+            return await _page.IsVisibleAsync(DashboardHeaderSelector);
+        }
+
+        // Directory Element Methods
+        public async Task GoToDirectoryPageAsync()
+        {
+            await _page.ClickAsync(DirectorySelector);
+        }
+        public async Task<bool> IsDirectoryHeaderVisibleAsync()
+        {
+            return await _page.IsVisibleAsync(DirectoryHeaderSelector);
+        }
+
+        // Maintenance Element Methods
+        public async Task GoToMaintenancePageAsync()
+        {
+            await _page.ClickAsync(MaintenanceSelector);
+        }
+        public async Task<bool> IsMaintenanceHeaderVisibleAsync()
+        {
+            return await _page.IsVisibleAsync(MaintenanceHeaderSelector);
+        }
+
+        // Claim Element Methods
+        public async Task GoToClaimPageAsync()
+        {
+            await _page.ClickAsync(ClaimSelector);
+        }
+        public async Task<bool> IsClaimHeaderVisibleAsync()
+        {
+            return await _page.IsVisibleAsync(ClaimHeaderSelector);
+        }
+
+        // Buzz Element Methods
+        public async Task GoToBuzzPageAsync()
+        {
+            await _page.ClickAsync(BuzzSelector);
+        }
+        public async Task<bool> IsBuzzHeaderVisibleAsync()
+        {
+            return await _page.IsVisibleAsync(BuzzHeaderSelector);
         }
     }
 }
--- Tests/NavigationPanelPageTests.cs
+++ Tests/NavigationPanelPageTests.cs
@@ -49,6 +49,22 @@
 
         [TestMethod]
         [TestCategory("PositiveTest")]
+        [TestCategory("TestPIM")]
+        [TestCategory("Navigation")]
+        public async Task TestPIMPageHeaderIsVisible()
+        {
+            _navigationPanelPage = new NavigationPanelPage(_page);
+            await _navigationPanelPage.GoToPIMPageAsync();
+
+            var pimHeaderText = await _page.InnerTextAsync(NavigationPanelPage.PIMHeaderSelector);
+            Console.WriteLine($"Actual PIM Header Text: {pimHeaderText}");
+
+            bool isPIMHeaderVisible = await _navigationPanelPage.IsPIMHeaderVisibleAsync();
+            Assert.IsTrue(isPIMHeaderVisible, "The PIM header is not visible.");
+        }
+
+        [TestMethod]
+        [TestCategory("PositiveTest")]
         [TestCategory("TestLeave")]
         [TestCategory("Navigation")]
         public async Task TestleavePageHeaderIsVisible()
@@ -63,21 +79,68 @@
             Assert.IsTrue(isLeaveHeaderVisible, "The Leave header is not visible.");
         }
 
+        [TestMethod]
+        [TestCategory("PositiveTest")]
+        [TestCategory("TestTime")]
+        [TestCategory("Navigation")]
+        public async Task TestTimePageHeaderIsVisible()
+        {
+            _navigationPanelPage = new NavigationPanelPage(_page);
+            await _navigationPanelPage.GoToTimePageAsync();
+
+            var timeHeaderText = await _page.InnerTextAsync(NavigationPanelPage.TimeHeaderSelector);
+            Console.WriteLine($"Actual Time Header Text: {timeHeaderText}");
+
+            bool isTimeHeaderVisible = await _navigationPanelPage.IsTimeHeaderVisibleAsync();
+            Assert.IsTrue(isTimeHeaderVisible, "The Time header is not visible.");
+        }
 
         [TestMethod]
         [TestCategory("PositiveTest")]
-        [TestCategory("TestDirectory")]
+        [TestCategory("TestRecruitment")]
         [TestCategory("Navigation")]
-        public async Task TestDirectoryPageHeaderIsVisible()
+        public async Task TestRecruitmentPageHeaderIsVisible()
         {
             _navigationPanelPage = new NavigationPanelPage(_page);
-            await _navigationPanelPage.GoToDirectoryPageAsync();
+            await _navigationPanelPage.GoToRecruitmentPageAsync();
 
-            var directoryHeaderText = await _page.InnerTextAsync(NavigationPanelPage.DirectoryHeaderSelector);
-            Console.WriteLine($"Actual Directory Header Text: {directoryHeaderText}");
+            var recruitmentHeaderText = await _page.InnerTextAsync(NavigationPanelPage.RecruitmentHeaderSelector);
+            Console.WriteLine($"Actual Recruitment Header Text: {recruitmentHeaderText}");
 
-            bool isDirectoryHeaderVisible = await _navigationPanelPage.IsDirectoryHeaderVisibleAsync();
-            Assert.IsTrue(isDirectoryHeaderVisible, "The Directory header is not visible.");
+            bool isRecruitmentHeaderVisible = await _navigationPanelPage.IsRecruitmentHeaderVisibleAsync();
+            Assert.IsTrue(isRecruitmentHeaderVisible, "The Recruitment header is not visible.");
+        }
+
+        [TestMethod]
+        [TestCategory("PositiveTest")]
+        [TestCategory("TestMyInfo")]
+        [TestCategory("Navigation")]
+        public async Task TestMyInfoPageHeaderIsVisible()
+        {
+            _navigationPanelPage = new NavigationPanelPage(_page);
+            await _navigationPanelPage.GoToMyInfoPageAsync();
+
+            var myInfoHeaderText = await _page.InnerTextAsync(NavigationPanelPage.MyInfoHeaderSelector);
+            Console.WriteLine($"Actual My Info Header Text: {myInfoHeaderText}");
+
+            bool isMyInfoHeaderVisible = await _navigationPanelPage.IsMyInfoHeaderVisibleAsync();
+            Assert.IsTrue(isMyInfoHeaderVisible, "The My Info header is not visible.");
+        }
+
+        [TestMethod]
+        [TestCategory("PositiveTest")]
+        [TestCategory("TestPerformance")]
+        [TestCategory("Navigation")]
+        public async Task TestPerformancePageHeaderIsVisible()
+        {
+            _navigationPanelPage = new NavigationPanelPage(_page);
+            await _navigationPanelPage.GoToPerformancePageAsync();
+
+            var performanceHeaderText = await _page.InnerTextAsync(NavigationPanelPage.PerformanceHeaderSelector);
+            Console.WriteLine($"Actual Performance Header Text: {performanceHeaderText}");
+
+            bool isPerformanceHeaderVisible = await _navigationPanelPage.IsPerformanceHeaderVisibleAsync();
+            Assert.IsTrue(isPerformanceHeaderVisible, "The Performance header is not visible.");
         }
 
         [TestMethod]
@@ -98,34 +161,66 @@
 
         [TestMethod]
         [TestCategory("PositiveTest")]
-        [TestCategory("TestPIM")]
+        [TestCategory("TestDirectory")]
         [TestCategory("Navigation")]
-        public async Task TestPIMPageHeaderIsVisible()
+        public async Task TestDirectoryPageHeaderIsVisible()
         {
             _navigationPanelPage = new NavigationPanelPage(_page);
-            await _navigationPanelPage.GoToPIMPageAsync();
+            await _navigationPanelPage.GoToDirectoryPageAsync();
 
-            var pimHeaderText = await _page.InnerTextAsync(NavigationPanelPage.PIMHeaderSelector);
-            Console.WriteLine($"Actual PIM Header Text: {pimHeaderText}");
+            var directoryHeaderText = await _page.InnerTextAsync(NavigationPanelPage.DirectoryHeaderSelector);
+            Console.WriteLine($"Actual Directory Header Text: {directoryHeaderText}");
 
-            bool isPIMHeaderVisible = await _navigationPanelPage.IsPIMHeaderVisibleAsync();
-            Assert.IsTrue(isPIMHeaderVisible, "The PIM header is not visible.");
+            bool isDirectoryHeaderVisible = await _navigationPanelPage.IsDirectoryHeaderVisibleAsync();
+            Assert.IsTrue(isDirectoryHeaderVisible, "The Directory header is not visible.");
         }
 
         [TestMethod]
         [TestCategory("PositiveTest")]
-        [TestCategory("TestTime")]
+        [TestCategory("TestMaintenance")]
         [TestCategory("Navigation")]
-        public async Task TestTimePageHeaderIsVisible()
+        public async Task TestMaintenancePageHeaderIsVisible()
         {
             _navigationPanelPage = new NavigationPanelPage(_page);
-            await _navigationPanelPage.GoToTimePageAsync();
+            await _navigationPanelPage.GoToMaintenancePageAsync();
 
-            var timeHeaderText = await _page.InnerTextAsync(NavigationPanelPage.TimeHeaderSelector);
-            Console.WriteLine($"Actual Time Header Text: {timeHeaderText}");
+            var maintenanceHeaderText = await _page.InnerTextAsync(NavigationPanelPage.MaintenanceHeaderSelector);
+            Console.WriteLine($"Actual Maintenance Header Text: {maintenanceHeaderText}");
 
-            bool isTimeHeaderVisible = await _navigationPanelPage.IsTimeHeaderVisibleAsync();
-            Assert.IsTrue(isTimeHeaderVisible, "The Time header is not visible.");
+            bool isMaintenanceHeaderVisible = await _navigationPanelPage.IsMaintenanceHeaderVisibleAsync();
+            Assert.IsTrue(isMaintenanceHeaderVisible, "The Maintenance header is not visible.");
+        }
+
+        [TestMethod]
+        [TestCategory("PositiveTest")]
+        [TestCategory("TestClaim")]
+        [TestCategory("Navigation")]
+        public async Task TestClaimPageHeaderIsVisible()
+        {
+            _navigationPanelPage = new NavigationPanelPage(_page);
+            await _navigationPanelPage.GoToClaimPageAsync();
+
+            var claimHeaderText = await _page.InnerTextAsync(NavigationPanelPage.ClaimHeaderSelector);
+            Console.WriteLine($"Actual Claim Header Text: {claimHeaderText}");
+
+            bool isClaimHeaderVisible = await _navigationPanelPage.IsClaimHeaderVisibleAsync();
+            Assert.IsTrue(isClaimHeaderVisible, "The Claim header is not visible.");
+        }
+
+        [TestMethod]
+        [TestCategory("PositiveTest")]
+        [TestCategory("TestBuzz")]
+        [TestCategory("Navigation")]
+        public async Task TestBuzzPageHeaderIsVisible()
+        {
+            _navigationPanelPage = new NavigationPanelPage(_page);
+            await _navigationPanelPage.GoToBuzzPageAsync();
+
+            var buzzHeaderText = await _page.InnerTextAsync(NavigationPanelPage.BuzzHeaderSelector);
+            Console.WriteLine($"Actual Buzz Header Text: {buzzHeaderText}");
+
+            bool isBuzzHeaderVisible = await _navigationPanelPage.IsBuzzHeaderVisibleAsync();
+            Assert.IsTrue(isBuzzHeaderVisible, "The Buzz header is not visible.");
         }
     }
 }
