e0da760a4dc8318009d1664c67e039fcaadd8f01
Noel Buxton <noel.buxton@mmc.com>
2023-10-03 14:07:08 +0100

    Refactored to use dictionary

--- Pages/PIMPage.cs
+++ Pages/PIMPage.cs
@@ -9,6 +9,7 @@
             _page = page;
         }
 
+        /*
         // Define selectors for elements on the PIM page
         private const string EmployeeListSelector = "a[class='oxd-topbar-body-nav-tab-item']:text('Employee List')";
         private const string AddEmployeeSelector = "a[class='oxd-topbar-body-nav-tab-item']:text('Add Employee')";
@@ -61,5 +62,36 @@
             return await _page.InnerTextAsync(PIMPage.ReportsSelector);
         }
         // Add more methods for other interactions and verifications as needed
+        */
+
+
+
+        public static Dictionary<string, string> elements = new Dictionary<string, string>() {
+            {"Employee List", ""},
+            {"Add Employee" , ""},
+            {"Reports"      , ""}
+        };
+
+        public static string getElementSelector(string element)
+        {
+            return elements.ContainsKey(element) ? $"a[class='oxd-topbar-body-nav-tab-item']:text('{element}')" : "";
+        }
+
+        // Methods to interact with elements on the page
+        public async Task GoToElementPageAsync(string element)
+        {
+            await _page.ClickAsync(getElementSelector(element));
+        }
+
+        // Example verification method
+        public async Task<bool> IsElementPageVisibleAsync(string element)
+        {
+            return await _page.IsVisibleAsync(getElementSelector(element));
+        }
+
+        public async Task<string> GetElementPageHeaderText(string element)
+        {
+            return await _page.InnerTextAsync(getElementSelector(element));
+        }
     }
 }
