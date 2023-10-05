ba3b13638b0f14fdec846e078c83c9925bd63a00
Noel Buxton <noel.buxton@mmc.com>
2023-10-05 17:30:50 +0100

    Added Dictionary to Claim page

---
+++ Pages/ClaimPage.cs
@@ -0,0 +1,89 @@
+﻿namespace OrangeHRMTest.Pages
+{
+    public class ClaimPage
+    {
+        private readonly IPage _page;
+
+        public ClaimPage(IPage page)
+        {
+            _page = page;
+        }
+
+        // The keys of this dictionary are all the options across the top of the page
+        // If the control isn't a dropdown then the corresponding value is an array containing 2 strings 0) the type of heading 1) the heading text
+        // If the type of heading is "" then "h6" is used
+        // If the heading text is "" then the key is used
+        // If the control is a drop down then the corresponding value is a second dictionary
+        // The keys of the second dictionary are the drop down options
+        // The corresponding values of the second dictionary are arrays containing 2 strings 0) the type of heading 1) the heading text
+        // If the type of heading is "" then "h6" is used
+        // If the heading text is "" then the key is used
+        public static Dictionary<string, object> elements = new Dictionary<string, object>() {
+            {"Configuration " , new Dictionary<string, string[]>{ { "Events", new string[] { "h5", "" } },
+                                                                  { "Expense Types", new string[] { "h5", "" } } } },
+            {"Submit Claim"   , new string[] { "", "Create Claim Request" } },
+            {"My Claims"      , new string[] { "h5", "" } },
+            {"Employee Claims", new string[] { "h5", "" } },
+            {"Assign Claim"   , new string[] { "", "Create Claim Request" } }
+        };
+
+        public static string getElementSelector(string element)
+        {
+            return elements.ContainsKey(element) ? $"{(elements[element] is Array ? "a" : "span")}[class='oxd-topbar-body-nav-tab-item']:text('{element}')" : "";
+        }
+
+        public static string getSubelementSelector(string element, string subelement)
+        {
+            string returnValue = "";
+            if (elements.ContainsKey(element) && elements[element] is not Array)
+            {
+                Dictionary<string, string[]> subelements = (Dictionary<string, string[]>) elements[element];
+                returnValue = subelements.ContainsKey(subelement) ? $"a[class='oxd-topbar-body-nav-tab-link']:text('{subelement}')" : "";
+            }
+            return returnValue;
+        }
+
+        public static string getSubelementHeaderSelector(string element, string subelement)
+        {
+            string returnValue = "";
+            if (elements.ContainsKey(element) && elements[element] is not Array)
+            {
+                Dictionary<string, string[]> subelements = (Dictionary<string, string[]>) elements[element];
+                if (subelements.ContainsKey(subelement))
+                {
+                    string[] details = subelements[subelement];
+                    returnValue = $"{(details[0] == "" ? "h6" : details[0])}.oxd-text:has-text('{(details[1] == "" ? subelement : details[1])}')";
+                }
+            }
+            return returnValue;
+        }
+
+        // Methods to interact with elements on the page
+        public async Task GoToElementPageAsync(string element)
+        {
+            await _page.ClickAsync(getElementSelector(element));
+        }
+
+        // Methods to interact with elements on the page
+        public async Task GoToSubelementPageAsync(string element, string subelement)
+        {
+            await _page.ClickAsync(getSubelementSelector(element, subelement));
+        }
+
+        // Example verification method
+        public async Task<bool> IsElementPageVisibleAsync(string element)
+        {
+            return await _page.IsVisibleAsync(getElementSelector(element));
+        }
+
+        public async Task<string> GetElementPageHeaderText(string element)
+        {
+            return await _page.InnerTextAsync(getElementSelector(element));
+        }
+
+        public async Task<string> GetSublementPageHeaderText(string element, string subelement)
+        {
+            return await _page.InnerTextAsync(getSubelementHeaderSelector(element, subelement));
+        }
+    }
+}
---
+++ Tests/ClaimPageTests.cs
@@ -0,0 +1,138 @@
+﻿using OrangeHRMTest.Pages;
+
+namespace OrangeHRMTest.Tests
+{
+    [TestClass]
+    public class ClaimPageTests
+    {
+        private IBrowser? _browser;
+        private IBrowserContext? _context;
+        private IPage? _page;
+        private LoginPage? _loginPage; // POM created for Login Page
+        private NavigationPanelPage? _navigationPanelPage; // POM created for the NavigationPanel
+        private ClaimPage? _ClaimPage; // POM created for the Claim Page
+        [TestInitialize]
+        public async Task Setup()
+        {
+            // Initialize browser, context, and page
+            var playwright = await Playwright.CreateAsync();
+
+            bool demo = false;
+            BrowserTypeLaunchOptions options = new BrowserTypeLaunchOptions();
+            if (demo)
+            {
+                options.Headless = false;
+                options.SlowMo = 1000;
+            }
+            _browser = await playwright.Chromium.LaunchAsync(options);
+
+            _context = await _browser.NewContextAsync();
+            _page = await _context.NewPageAsync();
+
+            // Navigate and log in to the application
+            _loginPage = new LoginPage(_page);
+            await _loginPage.GoToLoginPage();
+            await _loginPage.Login("admin", "admin123", expectSuccess: true);
+        }
+
+        [TestCleanup]
+        public async Task Cleanup()
+        {
+            // Close browser after tests
+            await _browser.CloseAsync();
+        }
+
+        [TestMethod]
+        [TestCategory("PositiveTest")]
+        [TestCategory("Claim Page Elements")]
+        [DataRow("Submit Claim")]
+        [DataRow("My Claims")]
+        [DataRow("Employee Claims")]
+        [DataRow("Assign Claim")]
+
+        public async Task TestElementPageVisible(string element)
+        {
+            _navigationPanelPage = new NavigationPanelPage(_page);
+            await _navigationPanelPage.GoToPageAsync("Claim");
+
+            // Create a ClaimPage object
+            _ClaimPage = new ClaimPage(_page);
+
+            // Navigate to the element page
+            await _ClaimPage.GoToElementPageAsync(element);
+
+            // Get the text on the element page header
+            var headerText = await _ClaimPage.GetElementPageHeaderText(element);
+            Console.WriteLine($"Actual Header Text: {headerText}");
+
+            // Perform verifications or interactions on the element page
+            bool isElementPageVisible = await _ClaimPage.IsElementPageVisibleAsync(element);
+            Assert.IsTrue(isElementPageVisible, $"The {element} page is not visible.");
+        }
+
+        [TestMethod]
+        [TestCategory("PositiveTest")]
+        [TestCategory("Claim Page Elements")]
+        [DataRow("Configuration ")]
+        [DataRow("Submit Claim")]
+        [DataRow("My Claims")]
+        [DataRow("Employee Claims")]
+        [DataRow("Assign Claim")]
+
+        public async Task TestElementPageVisible2(string element)
+        {
+            _navigationPanelPage = new NavigationPanelPage(_page);
+            await _navigationPanelPage.GoToPageAsync("Claim");
+
+            // Create a ClaimPage object
+            _ClaimPage = new ClaimPage(_page);
+
+            if (AdminPage.elements.ContainsKey(element))
+            {
+                if (AdminPage.elements[element] is Array)
+                {
+                    // Navigate to the element page
+                    await _ClaimPage.GoToElementPageAsync(element);
+
+                    // Get the text on the element page header
+                    var headerText = await _ClaimPage.GetElementPageHeaderText(element);
+                    Console.WriteLine($"Actual Header Text: {headerText}");
+                    Assert.AreEqual(element, headerText, false, $"The header {element} was not found.");
+
+                    // Perform verifications or interactions on the element page
+                    bool isElementPageVisible = await _ClaimPage.IsElementPageVisibleAsync(element);
+                    Assert.IsTrue(isElementPageVisible, $"The {element} page is not visible.");
+                }
+                else
+                {
+                    Console.WriteLine($"The element {element} has multiple choices.");
+                    Dictionary<string, string[]> subelements = (Dictionary<string, string[]>)AdminPage.elements[element];
+                    foreach (string subelement in subelements.Keys)
+                    {
+                        Console.WriteLine($"The sub-element is {subelement}.");
+
+                        // Navigate to the element page
+                        await _ClaimPage.GoToElementPageAsync(element);
+
+                        // Navigate to the sub-element
+                        await _ClaimPage.GoToSubelementPageAsync(element, subelement);
+
+                        // Get the text on the element page header
+                        var headerText = await _ClaimPage.GetSublementPageHeaderText(element, subelement);
+                        Console.WriteLine($"Actual Header Text: {headerText}");
+                        string expected = subelements[subelement][1] == "" ? subelement : subelements[subelement][1];
+                        Assert.AreEqual(expected, headerText, false, $"The header {expected} was not found.");
+
+                        // Perform verifications or interactions on the element page
+                        bool isElementPageVisible = await _ClaimPage.IsElementPageVisibleAsync(element);
+                        Assert.IsTrue(isElementPageVisible, $"The {element} page is not visible.");
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine($"The element {element} does not exist on the Admin page.");
+            }
+        }
+    }
+}
