3e3f45056dba49e6379a9378284563ecedc91f89
Noel Buxton <noel.buxton@mmc.com>
2023-10-05 17:19:14 +0100

    Added Dictionary to Performance page

---
+++ Pages/PerformancePage.cs
@@ -0,0 +1,90 @@
+﻿namespace OrangeHRMTest.Pages
+{
+    public class PerformancePage
+    {
+        private readonly IPage _page;
+
+        public PerformancePage(IPage page)
+        {
+            _page = page;
+        }
+
+        // The keys of this dictionary are all the options across the top of the page
+        // If the control isn't a dropdown then the corresponding value is an array containing 2 strings 0) the type of heading 1) the heading text
+        // If the type of heading is "" then "h6" is used
+        // If the heading text is "" then the key is used
+        // If the control is a drop down then the corresponding value is a second dictionary
+        // The keys of the second dictionary are the drop down options
+        // The corresponding values of the second dictionary are arrays containing 2 strings 0) the type of heading 1) the heading text
+        // If the type of heading is "" then "h6" is used
+        // If the heading text is "" then the key is used
+        public static Dictionary<string, object> elements = new Dictionary<string, object>() {
+            {"Configure "       , new Dictionary<string, string[]>{ { "KPIs", new string[] { "h5", "Key Performance Indicators for Job Title" } },
+                                                                    { "Trackers", new string[] { "h5", "Performance Trackers" } } } },
+            {"Manage Reviews "  , new Dictionary<string, string[]>{ { "Manage Reviews", new string[] { "h5", "Manage Performance Reviews" } },
+                                                                    { "My Reviews", new string[] { "", "" } },
+                                                                    { "Employee Reviews", new string[] { "h5", "" } } } },
+            {"My Trackers"      , new string[] { "", "My Performance Trackers" } },
+            {"Employee Trackers", new string[] { "h5", "Employee Performance Trackers" } }
+        };
+
+        public static string getElementSelector(string element)
+        {
+            return elements.ContainsKey(element) ? $"{(elements[element] is Array ? "a" : "span")}[class='oxd-topbar-body-nav-tab-item']:text('{element}')" : "";
+        }
+
+        public static string getSubelementSelector(string element, string subelement)
+        {
+            string returnValue = "";
+            if (elements.ContainsKey(element) && elements[element] is not Array)
+            {
+                Dictionary<string, string[]> subelements = (Dictionary<string, string[]>) elements[element];
+                returnValue = subelements.ContainsKey(subelement) ? $"a[class='oxd-topbar-body-nav-tab-link']:text('{subelement}')" : "";
+            }
+            return returnValue;
+        }
+
+        public static string getSubelementHeaderSelector(string element, string subelement)
+        {
+            string returnValue = "";
+            if (elements.ContainsKey(element) && elements[element] is not Array)
+            {
+                Dictionary<string, string[]> subelements = (Dictionary<string, string[]>) elements[element];
+                if (subelements.ContainsKey(subelement))
+                {
+                    string[] details = subelements[subelement];
+                    returnValue = $"{(details[0] == "" ? "h6" : details[0])}.oxd-text:has-text('{(details[1] == "" ? subelement : details[1])}')";
+                }
+            }
+            return returnValue;
+        }
+
+        // Methods to interact with elements on the page
+        public async Task GoToElementPageAsync(string element)
+        {
+            await _page.ClickAsync(getElementSelector(element));
+        }
+
+        // Methods to interact with elements on the page
+        public async Task GoToSubelementPageAsync(string element, string subelement)
+        {
+            await _page.ClickAsync(getSubelementSelector(element, subelement));
+        }
+
+        // Example verification method
+        public async Task<bool> IsElementPageVisibleAsync(string element)
+        {
+            return await _page.IsVisibleAsync(getElementSelector(element));
+        }
+
+        public async Task<string> GetElementPageHeaderText(string element)
+        {
+            return await _page.InnerTextAsync(getElementSelector(element));
+        }
+
+        public async Task<string> GetSublementPageHeaderText(string element, string subelement)
+        {
+            return await _page.InnerTextAsync(getSubelementHeaderSelector(element, subelement));
+        }
+    }
+}
---
+++ Tests/PerformancePageTests.cs
@@ -0,0 +1,135 @@
+﻿using OrangeHRMTest.Pages;
+
+namespace OrangeHRMTest.Tests
+{
+    [TestClass]
+    public class PerformancePageTests
+    {
+        private IBrowser? _browser;
+        private IBrowserContext? _context;
+        private IPage? _page;
+        private LoginPage? _loginPage; // POM created for Login Page
+        private NavigationPanelPage? _navigationPanelPage; // POM created for the NavigationPanel
+        private PerformancePage? _PerformancePage; // POM created for the Performance Page
+        [TestInitialize]
+        public async Task Setup()
+        {
+            // Initialize browser, context, and page
+            var playwright = await Playwright.CreateAsync();
+
+            bool demo = false;
+            BrowserTypeLaunchOptions options = new BrowserTypeLaunchOptions();
+            if (demo)
+            {
+                options.Headless = false;
+                options.SlowMo = 1000;
+            }
+            _browser = await playwright.Chromium.LaunchAsync(options);
+
+            _context = await _browser.NewContextAsync();
+            _page = await _context.NewPageAsync();
+
+            // Navigate and log in to the application
+            _loginPage = new LoginPage(_page);
+            await _loginPage.GoToLoginPage();
+            await _loginPage.Login("admin", "admin123", expectSuccess: true);
+        }
+
+        [TestCleanup]
+        public async Task Cleanup()
+        {
+            // Close browser after tests
+            await _browser.CloseAsync();
+        }
+
+        [TestMethod]
+        [TestCategory("PositiveTest")]
+        [TestCategory("Performance Page Elements")]
+        [DataRow("My Trackers")]        
+        [DataRow("Employee Trackers")]
+
+        public async Task TestElementPageVisible(string element)
+        {
+            _navigationPanelPage = new NavigationPanelPage(_page);
+            await _navigationPanelPage.GoToPageAsync("Performance");
+
+            // Create a PerformancePage object
+            _PerformancePage = new PerformancePage(_page);
+
+            // Navigate to the element page
+            await _PerformancePage.GoToElementPageAsync(element);
+
+            // Get the text on the element page header
+            var headerText = await _PerformancePage.GetElementPageHeaderText(element);
+            Console.WriteLine($"Actual Header Text: {headerText}");
+
+            // Perform verifications or interactions on the element page
+            bool isElementPageVisible = await _PerformancePage.IsElementPageVisibleAsync(element);
+            Assert.IsTrue(isElementPageVisible, $"The {element} page is not visible.");
+        }
+
+        [TestMethod]
+        [TestCategory("PositiveTest")]
+        [TestCategory("Performance Page Elements")]
+        [DataRow("Configure ")]
+        [DataRow("Manage Reviews ")]
+        [DataRow("My Trackers")]
+        [DataRow("Employee Trackers")]
+
+        public async Task TestElementPageVisible2(string element)
+        {
+            _navigationPanelPage = new NavigationPanelPage(_page);
+            await _navigationPanelPage.GoToPageAsync("Performance");
+
+            // Create a PerformancePage object
+            _PerformancePage = new PerformancePage(_page);
+
+            if (AdminPage.elements.ContainsKey(element))
+            {
+                if (AdminPage.elements[element] is Array)
+                {
+                    // Navigate to the element page
+                    await _PerformancePage.GoToElementPageAsync(element);
+
+                    // Get the text on the element page header
+                    var headerText = await _PerformancePage.GetElementPageHeaderText(element);
+                    Console.WriteLine($"Actual Header Text: {headerText}");
+                    Assert.AreEqual(element, headerText, false, $"The header {element} was not found.");
+
+                    // Perform verifications or interactions on the element page
+                    bool isElementPageVisible = await _PerformancePage.IsElementPageVisibleAsync(element);
+                    Assert.IsTrue(isElementPageVisible, $"The {element} page is not visible.");
+                }
+                else
+                {
+                    Console.WriteLine($"The element {element} has multiple choices.");
+                    Dictionary<string, string[]> subelements = (Dictionary<string, string[]>)AdminPage.elements[element];
+                    foreach (string subelement in subelements.Keys)
+                    {
+                        Console.WriteLine($"The sub-element is {subelement}.");
+
+                        // Navigate to the element page
+                        await _PerformancePage.GoToElementPageAsync(element);
+
+                        // Navigate to the sub-element
+                        await _PerformancePage.GoToSubelementPageAsync(element, subelement);
+
+                        // Get the text on the element page header
+                        var headerText = await _PerformancePage.GetSublementPageHeaderText(element, subelement);
+                        Console.WriteLine($"Actual Header Text: {headerText}");
+                        string expected = subelements[subelement][1] == "" ? subelement : subelements[subelement][1];
+                        Assert.AreEqual(expected, headerText, false, $"The header {expected} was not found.");
+
+                        // Perform verifications or interactions on the element page
+                        bool isElementPageVisible = await _PerformancePage.IsElementPageVisibleAsync(element);
+                        Assert.IsTrue(isElementPageVisible, $"The {element} page is not visible.");
+                    }
+                }
+            }
+            else
+            {
+                Console.WriteLine($"The element {element} does not exist on the Admin page.");
+            }
+        }
+    }
+}
