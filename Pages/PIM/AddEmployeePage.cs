43e22b20d75a8c1d2ad8a86cf59d99314ca89aa9
mohit-ahuja <mohit.ahuja@jltgroupcloud.com>
2023-10-12 16:15:25 +0530

    PIMPageTests

--- Pages/PIM/AddEmployeePage.cs
+++ Pages/PIM/AddEmployeePage.cs
@@ -14,19 +14,33 @@
         private const string LastNameSelector = "input[name='lastName']";
         private const string EmailSelector = "input[name='email']";
         private const string SubmitButtonSelector = "button[type='submit']";
+        private const string UpdateFirstNameSelector = "input[placeholder='Type for hints...']";
+        private const string DeleteButtonSelector = "button[class='oxd-icon-button oxd-table-cell-action-space']";
 
         // Methods to interact with elements
-        public async Task FillOutEmployeeDetailsAsync(string firstName, string lastName, string email)
+        public async Task FillOutEmployeeDetailsAsync(string firstName, string lastName)
         {
             await _page.FillAsync(FirstNameSelector, firstName);
             await _page.FillAsync(LastNameSelector, lastName);
-            await _page.FillAsync(EmailSelector, email);
+            //await _page.FillAsync(EmailSelector, email);
         }
 
         public async Task SubmitFormAsync()
         {
             await _page.ClickAsync(SubmitButtonSelector);
         }
-        // Add any additional methods or verifications as needed
+        public async Task DeleteButtonAsync()
+        {
+            await _page.ClickAsync(DeleteButtonSelector);
+        }
+        public async Task UpdateEmployeeDetailsAsync(string firstName)
+        {
+            await _page.FillAsync(UpdateFirstNameSelector, firstName);
+        }
+
+        public async Task FillOutEditEmployeeDetailsAsync(string firstName1)
+        {
+            await _page.FillAsync(FirstNameSelector, firstName1);
+        }
     }
 }
